307cde66a6ac3b71f29994e56f5e8e1236e3c061
Nhattu2604 <nguyennhattucr01ld@gmail.com>
2025-02-17 14:55:02 +0700

    first commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,33 @@
+
+	for event in pygame.event.get():
+		#quit game
+		if event.type == pygame.QUIT:
+			run = False
+		#keyboard presses
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_a:
+				moving_left = True
+			if event.key == pygame.K_d:
+				moving_right = True
+			if event.key == pygame.K_SPACE:
+				shoot = True
+			if event.key == pygame.K_q:
+				grenade = True
+			if event.key == pygame.K_w and player.alive:
+				player.jump = True
+				jump_fx.play()
+			if event.key == pygame.K_ESCAPE:
+				run = False
+
+
+		#keyboard button released
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_a:
+				moving_left = False
+			if event.key == pygame.K_d:
+				moving_right = False
+			if event.key == pygame.K_SPACE:
+				shoot = False
+			if event.key == pygame.K_q:
+				grenade = False
+				grenade_thrown = False
